159ba0cd8240f0b2fc5457f06853d491d5badbe2
Huni <huni971209@naver.com>
2025-07-01 17:47:55 +0900

    <springboot> 스프링부트 MyBatis 적용 및 테스팅

---
+++ SpringMyBatisHomework/src/main/java/com/kh/SpringMyBatisHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyBatisHomeworkApplication.class, args);
+	}
+
+}
